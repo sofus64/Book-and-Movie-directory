bc7de1d547f78f130cc53c241c066e5395b06309
sofus64 <157691346+sofus64@users.noreply.github.com>
2024-05-24 13:45:09 +0200

    Add project files.

---
+++ Parprogrammering/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parprogrammering
+{
+    internal class Book
+    {
+        public string Title;
+        public string Author;
+        public string Description;
+
+        public Book(string title, string author, string description)
+        {
+            Title = title;
+            Author = author;
+            Description = description;
+        }
+    }
+}
---
+++ Parprogrammering/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parprogrammering
+{
+    internal class Movie
+    {
+        public string Title;
+        public string Description;
+        public string Director;
+        public List<string> Actors;
+
+        public Movie(string title, string description, string director, List<string> actors)
+        {
+            Title = title;
+            Description = description;
+            Director = director;
+            Actors = actors;
+        }
+    }
+       
+}
